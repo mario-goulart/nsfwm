564acef1166b5ad0a8c586a1c358cf03bf70b01a
Mario Domenech Goulart <mario.goulart@gmail.com>
2015-05-16 15:32:54 -0300

    button-press: raise window, unless window is root

--- nsfwm.scm
+++ nsfwm.scm
@@ -652,6 +652,8 @@ XSetErrorHandler(ignore_xerror);
                           (clean-mask (xbuttonevent-state ev)))))
               buttons)))
         (when button
+          (unless (root-window? window)
+            (raise-window! window))
           ((button-procedure button)))))))
 
 (vector-set! handlers BUTTONPRESS button-press)
