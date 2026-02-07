mario-goulart/nsfwm

commit de577d874e3467eb93035c705bad4e46d7db2a08

Resizing a maximized window causes it to become unmaximized
Reset window's original position/dimensions when resizing a maximized
window, otherwise maximizing a previously maximized window that had
been resized would cause it to restore the original
position/dimensions values instead of actually maximizing it.

nsfwm.scm
@@ -574,14 +574,16 @@ XSetErrorHandler(ignore_xerror);
       (show-window! window)))
 
 (define (resize-window! window width height)
-  (let* ((wid (window-id window))
-         (border-width (fx* 2 (window-border-width window)))
+  ;; Resizing a maximized window causes it to become unmaximized
+  (when (window-maximized? window)
+    (reset-window-orig-position/dimensions! window))
+  (let* ((border-width (fx* 2 (window-border-width window)))
          (new-width (fx- width border-width))
          (new-height (fx- height border-width)))
     (when (and (fx> new-width 0) (fx> new-height 0))
       (window-width-set! window width)
       (window-height-set! window height)
-      (xresizewindow *dpy* wid new-width new-height))))
+      (xresizewindow *dpy* (window-id window) new-width new-height))))
 
 (define (resize-window-to-pointer-position! #!optional window)
   ;; Grow/shrink windows my moving corners in the direction of the
@@ -686,6 +688,12 @@ XSetErrorHandler(ignore_xerror);
       (unmaximize-window! window)
       (maximize-window-vertically! window)))
 
+(define (reset-window-orig-position/dimensions! window)
+  (window-orig-position-x-set! window #f)
+  (window-orig-position-y-set! window #f)
+  (window-orig-width-set! window #f)
+  (window-orig-height-set! window #f))
+
 (define (unmaximize-window! window)
   ;; Resize and move
   (let ((orig-border-width (window-orig-border-width window)))
@@ -703,10 +711,7 @@ XSetErrorHandler(ignore_xerror);
                     (window-position-y window)))
   ;; Reset orig geometry, so window-maximize? knows whether the
   ;; window is maximized or not
-  (window-orig-position-x-set! window #f)
-  (window-orig-position-y-set! window #f)
-  (window-orig-width-set! window #f)
-  (window-orig-height-set! window #f))
+  (reset-window-orig-position/dimensions! window))
 
 (define (window-maximized? window)
   ;; When the orig geometry attributes are #f, window is not maximized.

commit c594e0cd28fa927b7e18e2efa26d7aaef828f631

Fix maximizing/unmaximizing operations
Issues introduced in de577d8, related to resetting orig properties in
resize-window!. Maximizing operations need to save origs before
calling resize-window!, then setting window's orig properties _after_
the resize-window! call.

nsfwm.scm
@@ -651,18 +651,28 @@ XSetErrorHandler(ignore_xerror);
          (new-x (car maximized-area))
          (new-y (cadr maximized-area))
          (new-width (caddr maximized-area))
-         (new-height (cadddr maximized-area)))
-    ;; Save current window geometry in the window object itself
-    (window-orig-position-x-set! window (window-position-x window))
-    (window-orig-position-y-set! window (window-position-y window))
-    (window-orig-width-set! window (window-width window))
-    (window-orig-height-set! window (window-height window))
-    (window-orig-border-width-set! window (or (window-border-width window)
-                                              (default-window-border-width)))
+         (new-height (cadddr maximized-area))
+         ;; Save current dimensions/positions to set the orig
+         ;; properties after maximizing
+         (orig-border-width (window-border-width window))
+         (orig-x (window-position-x window))
+         (orig-y (window-position-y window))
+         (orig-width (window-width window))
+         (orig-height (window-height window)))
     (when remove-border?
       (set-window-decoration! window border-width: 0))
+    (reset-window-orig-position/dimensions! window)
     ;; Actually maximize
     (resize-window! window new-width new-height)
+
+    ;; Save current window geometry in the window object itself (this
+    ;; must be done after resizing, as resize-window! checks the orig
+    ;; properties to determine whether the window is maximized.
+    (window-orig-position-x-set! window orig-x)
+    (window-orig-position-y-set! window orig-y)
+    (window-orig-width-set! window orig-width)
+    (window-orig-height-set! window orig-height)
+    (window-orig-border-width-set! window orig-border-width)
     (move-window! window new-x new-y)))
 
 (define (maximize-window-vertically! window)
@@ -675,12 +685,19 @@ XSetErrorHandler(ignore_xerror);
                       (x-get-geometry-info-width root-info)
                       (x-get-geometry-info-height root-info)))))
          (new-y (cadr maximized-area))
-         (new-height (cadddr maximized-area)))
-    ;; Save current window geometry in the window object itself
-    (window-orig-position-y-set! window (window-position-y window))
-    (window-orig-height-set! window (window-height window))
+         (new-height (cadddr maximized-area))
+         ;; Save current y position and height to set the orig
+         ;; properties after maximizing
+         (orig-y (window-position-y window))
+         (orig-height (window-height window)))
+
     ;; Actually maximize vertically
     (resize-window! window (window-width window) new-height)
+
+    ;; Save current window geometry in the window object itself
+    (window-orig-position-y-set! window orig-y)
+    (window-orig-height-set! window orig-height)
+
     (move-window! window (window-position-x window) new-y)))
 
 (define (toggle-maximize-window-vertically! window)
@@ -695,29 +712,31 @@ XSetErrorHandler(ignore_xerror);
   (window-orig-height-set! window #f))
 
 (define (unmaximize-window! window)
-  ;; Resize and move
-  (let ((orig-border-width (window-orig-border-width window)))
+  ;; Resize and move.  Save orig properties, as resize-window! will
+  ;; reset them.
+  (let ((orig-border-width (window-orig-border-width window))
+        (orig-x (window-orig-position-x window))
+        (orig-y (window-orig-position-y window))
+        (orig-width (window-orig-width window))
+        (orig-height (window-orig-height window)))
     (when orig-border-width
-      (set-window-decoration! window border-width: orig-border-width)))
-  (resize-window! window
-                  (or (window-orig-width window)
-                      (window-width window))
-                  (or (window-orig-height window)
-                      (window-height window)))
-  (move-window! window
-                (or (window-orig-position-x window)
-                    (window-position-x window))
-                (or (window-orig-position-y window)
-                    (window-position-y window)))
-  ;; Reset orig geometry, so window-maximize? knows whether the
-  ;; window is maximized or not
-  (reset-window-orig-position/dimensions! window))
+      (set-window-decoration! window border-width: orig-border-width))
+    (resize-window! window
+                    (or orig-width (window-width window))
+                    (or orig-height (window-height window)))
+    (move-window! window
+                  (or orig-x (window-position-x window))
+                  (or orig-y (window-position-y window)))
+    ;; Reset orig geometry, so window-maximize? knows whether the
+    ;; window is maximized or not
+    (reset-window-orig-position/dimensions! window)))
 
 (define (window-maximized? window)
   ;; When the orig geometry attributes are #f, window is not maximized.
-  (or (window-orig-position-x window)
-      ;; Window is maximized vertically
-      (window-orig-position-y window)))
+  (and (or (window-orig-position-x window)
+           ;; Window is maximized vertically
+           (window-orig-position-y window))
+       #t))
 
 (define (toggle-maximize-window! window #!key remove-border?)
   (if (window-maximized? window)